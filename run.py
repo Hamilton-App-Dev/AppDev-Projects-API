de51d579bfefa898bbe7f6a6b6d15e916867420d
Dominic Mauretti <dominicmauretti@dominics-mbp.hamilton.edu>
2023-11-14 15:59:21 -0500

    Connect

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://appdev:4W9w5paZIpXv8Gw8LfhmhHAoh811Q8rL@dpg-cl8a0uavokcc73ate250-a.oregon-postgres.render.com/db_lgx2'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(128))
+    more = db.Column(db.String(128))
 
 @app.route("/")
 def hello():
