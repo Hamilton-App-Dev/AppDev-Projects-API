af74d9f0f66ab4caa3997a10fe2ad6c6019dce36
Dominic Mauretti <dominicmauretti@dominics-mbp.hamilton.edu>
2023-11-28 15:28:50 -0500

    Changed app to run

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+run = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://appdev:4W9w5paZIpXv8Gw8LfhmhHAoh811Q8rL@dpg-cl8a0uavokcc73ate250-a.oregon-postgres.render.com/db_lgx2'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+run.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://appdev:4W9w5paZIpXv8Gw8LfhmhHAoh811Q8rL@dpg-cl8a0uavokcc73ate250-a.oregon-postgres.render.com/db_lgx2'
+db = SQLAlchemy(run)
+migrate = Migrate(run, db)
 
 class Projects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,9 +14,16 @@ class Projects(db.Model):
     description = db.Column(db.String(128))
     more = db.Column(db.String(128))
 
-@app.route("/")
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    class_year = db.Column(db.Integer)
+    role = db.Column(db.String(128))
+    linkedin_url = db.Column(db.String(128))
+    
+@run.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8000)
+    run.run(host="localhost", port=8000)
